bde9853d4242a4b060f7169c955b4785dfef1ea0
sarthak-smartq <sarthak.gupta@thesmartq.com>
2025-11-30 22:43:31 +0530

    update .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",   // MUST match backend host
+  baseURL: import.meta.env.VITE_API_URL,   // MUST match backend host
   withCredentials: true,              // ⬅️ enables cookies globally
 });
 
